6e5e995b09261a20f9cc9fc4ecf3fd477e04b8ad
aidsy123 <39599835+aidsy123@users.noreply.github.com>
2018-05-24 14:40:34 -0400

    Add files via upload

---
+++ PoisonSnake.java
@@ -0,0 +1,23 @@
+class PoisonSnake extends Type{
+  
+ private String newType;
+  
+      PoisonSnake(int health, int strength, int intelligence, int defence, int level, int accuracy){
+    super(health, strength, intelligence, defence, level, accuracy);
+    newType = "poison";
+    setType(newType);
+  }
+      
+      
+      public void attack(int strength, int intelligence){
+      //if enemy hits the player, deal 10% damage to player(Tentative) and 
+      //deal a poison effect causing 1 damage over time for 5 hits
+      //once it attacks it will wait 1 second to attack again
+  }
+      
+      
+      public void move(){
+        //enemy moves in a random way until it sees the player
+        //the enemy chases after the player and attacks them
+      }
+}
