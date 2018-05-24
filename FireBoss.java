52ff25bc34ba0450261e9b059719315fd1a23060
aidsy123 <39599835+aidsy123@users.noreply.github.com>
2018-05-24 15:28:10 -0400

    Add files via upload

---
+++ FireBoss.java
@@ -0,0 +1,28 @@
+class FireBoss extends Enemy{
+  
+  private String newType;
+  Private final String name = "The Volcano";
+  
+      FireSpider(int health, int strength, int intelligence, int defence, int level, int accuracy){
+    super(health, strength, intelligence, defence, level, accuracy);
+     newType = "fire";
+    setType(newType);
+  }
+      
+      
+      public void attack(int strength, int intelligence){
+      //if enemy hits the player, deal 25% damage to player(Tentative) and 
+      //deal a burn effect causing 2 damage over time for 5 hits
+      //once it attacks once it will wait 2 second to attack again
+  }
+      
+      
+      public void move(){
+        //The Volvano moves towards the enemy slowly
+        //if he is close to the player, he will melee attack
+        //if he is farther from the player, he will used a ranged attack
+        //only ranged attack applies burn effect
+      }
+}
+  
+  
