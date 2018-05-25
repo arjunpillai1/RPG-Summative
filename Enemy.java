03f5e94703ea95251a08f9ceabf37400fb423a06
aidsy123 <39599835+aidsy123@users.noreply.github.com>
2018-05-25 14:24:43 -0400

    Add files via upload

--- Enemy.java
+++ Enemy.java
@@ -1,4 +1,4 @@
-abstract class Enemy extends Character{
+abstract class Enemy extends CombatCharacter{
   
   Enemy(int health, int strength, int intelligence, int defence, int level, int accuracy){
     super(health, strength, intelligence, defence, level, accuracy);
