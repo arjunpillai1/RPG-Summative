2285a2e972a1d5f371a6135ff265e95fef3def94
aidsy123 <39599835+aidsy123@users.noreply.github.com>
2018-05-28 14:28:18 -0400

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -1,10 +1,4 @@
-abstract class Enemy extends CombatCharacter{
-  
-  
-  
-  Enemy(int health, int strength, int intelligence, int defence, int level, int accuracy, String name){
-    super(health, strength, intelligence, defence, level, accuracy, name);
-  }
+
   // imports
 import java.util.Random;
 
