c012e3aaeba1bf5024374401ba9720049ee87af7
AlbertQuon <albertq2001@hotmail.com>
2018-06-14 13:50:37 -0400

    Update Archer.java

--- Archer.java
+++ Archer.java
@@ -1,32 +1,29 @@
-/* class Archer
- * Aiden Gimpel
- * June 14th, 2018
- * object for the archer enemy
-*/  
+/* [Archer.java]
+ * Enemy archer
+ * @author Aiden
+ * 06/14/2018
+ */
 class Archer extends Normal{
   
   private int newLvl;
+  private int imageChoice;
   
+  /**
+   * Archer constructor
+   * @param health, strength, intelligence, defence, level, accuracy, the name, intial position x and y, initial ground
+   * of enemy spawns
+   */
   Archer(int health, int strength, int intelligence, int defence, int level, int accuracy,
          String name, int posX, int posY , World initialGround){
     super(health, strength, intelligence, defence, level, accuracy, name, posX, posY, initialGround);
     newLvl = ((int)(Math.random()*2)+1);
     setLvl(newLvl);
+    imageChoice=((int)(Math.random()*3))*4;//0,4,8
   }
   
   
-  
-  /**
-* move
-* This method accepts a world, x Coord and a y coord as a parameter and moves the archer.
-* method returns a void
-* @param A  world array that holds data representing the game map
-* @param B integer holding the x coordinate of the enemy
-* @param C integer holding the y coordinate of the enemy
-* @return void
-*/
-  public void move(){
-    //enemy moves in a random way until it sees the player
-    //enemy attacks the player when in range
+  public int getImageChoice(){
+   return this.imageChoice; 
   }
+  
 }
