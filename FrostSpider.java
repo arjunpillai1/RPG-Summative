666fc5d1d250164650ee9bf563a6416b11888ff3
AlbertQuon <albertq2001@hotmail.com>
2018-06-14 13:56:45 -0400

    Update FrostSpider.java

--- FrostSpider.java
+++ FrostSpider.java
@@ -1,10 +1,14 @@
-/* class FrostSpider
- * Aiden Gimpel
- * June 14th, 2018
- * object for the frost spider enemy
-*/
+/*
+ * [FrostSpider.java]
+ * The frost snake enemy
+ * @author Albert
+ * 05/30/2018
+ */
 class FrostSpider extends Type{
-  
+  /**
+   * Constructor for frost spider
+   * @param health, strength, intelligence, defence, level, accuracy, name, initial position x and y, initial spawn ground
+   */
   FrostSpider(int health, int strength, int intelligence, int defence, int level, int accuracy, 
            String name,  int posX, int posY, World initialGround){
     super(health, strength, intelligence, defence, level, accuracy,"frost", name, posX, posY, initialGround);
@@ -12,17 +16,6 @@ class FrostSpider extends Type{
     setLvl(((int)(Math.random()*4)+6));
   }
   
-      /**
-* attack
-* This method accepts a world object and allows the enemy to attack the player
-* the method returns a void
-* @param A  world array that holds data representing the game map
-* @return void
-*/
-  public void attack(int strength, int intelligence){
-    //if enemy hits the player, deal 10% damage to player(Tentative) and 
-    //deal a burn effect causing  1 damage over time for 5 hits
-    //once it attacks once it will wait 1 second to attack again
-  }
+
   
 }
