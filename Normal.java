46794baa2f9d21474de6cc4369ab07b690d6ba88
AlbertQuon <albertq2001@hotmail.com>
2018-06-14 13:59:48 -0400

    Update Normal.java

--- Normal.java
+++ Normal.java
@@ -1,11 +1,16 @@
-/* class Normal
- * Aiden Gimpel
- * June 14th, 2018
- * abstract object for the normal typed enemies such as archer and bandit
-*/
+/*
+ * [Normal.java]
+ * Normal enemy type
+ * @author Aiden
+ * 05/25/2018
+ */
+
 abstract class Normal extends Enemy{
   
-  //constructor to make a normal enemy (Archer/Bandit)
+  /**
+   * Normal type constructor
+   * @param health, strength, intelligence, defence, level, accuracy, name, initial enemy x and y, initial spawn ground
+   */
     Normal(int health, int strength, int intelligence, int defence, int level, int accuracy, 
            String name,  int posX, int posY, World initialGround){
     super(health, strength, intelligence, defence, level, accuracy, name, posX, posY, initialGround);
