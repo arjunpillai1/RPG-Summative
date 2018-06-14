9ae68df2ae01b6085da14e3ccb69f39ea4aa615a
AlbertQuon <albertq2001@hotmail.com>
2018-06-14 13:53:12 -0400

    Update FireBoss.java

--- FireBoss.java
+++ FireBoss.java
@@ -1,37 +1,37 @@
-/* class FireBoss
- * Aiden Gimpel
- * June 14th, 2018
- * object for the Fire typed boss
-*/
+/*
+ * [FireBoss.java]
+ * The Fire boss, related to enemies
+ * @author Albert, Aiden
+ * 05/28/2018
+ */
 class FireBoss extends Type{
-    
+  /**
+   * Constructor for the fire boss
+   * @param health, strength, intelligence, defense, level, accuracy, name, initial position x and y, the initial world object
+   * spawned in on
+   */
   FireBoss(int health, int strength, int intelligence, int defence, int level, int accuracy, 
            String name,  int posX, int posY, World initialGround){
     super(health, strength, intelligence, defence, level, accuracy,"fire", "The Volcano", posX, posY
-         , initialGround);
+            , initialGround);
   }
   
   
-  public void attack(int strength, int intelligence){
-    //if enemy hits the player, deal 25% damage to player(Tentative) and 
-    //deal a burn effect causing 2 damage over time for 5 hits
-    //once it attacks once it will wait 2 second to attack again
-  }
   
-      /**
-* move
-* This method accepts a world, x Coord and a y coord as a parameter and moves the enemy.
-* method returns a void
-* @param A  world array that holds data representing the game map
-* @param B integer holding the x coordinate of the enemy
-* @param C integer holding the y coordinate of the enemy
-* @return void
-*/
-  public void move(){
-    //The Volcano moves towards the enemy slowly
-    //if he is close to the player, he will melee attack
-    //if he is farther from the player, he will used a ranged attack
-    //only ranged attack applies burn effect
+  /**
+   * Interaction method for the enemy
+   * @param the world array, enemy position 
+   */
+  @Override
+  public void move(World[][] world, int coordX, int coordY){
+    for (int i=coordX-2; i < coordX+3; i++) {
+      for (int j=coordY-2; j < coordY+3; j++) {
+        if (world[i][j] instanceof Player) {
+          attack(world[i][j]);
+          return;
+        }
+      }
+    }
   }
 }
 
