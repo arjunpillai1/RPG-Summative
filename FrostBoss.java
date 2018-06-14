b3a13e18bf1fe652fa34d1dcf4807dd6149dbdae
AlbertQuon <albertq2001@hotmail.com>
2018-06-14 13:55:01 -0400

    Update FrostBoss.java

--- FrostBoss.java
+++ FrostBoss.java
@@ -1,41 +1,36 @@
-/* class FrostBoss
- * Aiden Gimpel
- * June 14th, 2018
- * object for the frost typed boss
-*/
+/*
+ * [FrostBoss.java]
+ * The Frost boss enemy
+ * @author Albert, Aiden 
+ * 05/30/2018
+ */
+
+
 class FrostBoss extends Type{
 
-
+  /**
+   * Constructor for frost boss
+   * @param health, strength, intelligence, defence, level, accuracy, name, initial x and y, intial spawn ground
+   */
     FrostBoss(int health, int strength, int intelligence, int defence, int level, int accuracy, 
            String name,  int posX, int posY, World initialGround){
-        super(health, strength, intelligence, defence, level, accuracy,"frost"/*type*/ , 
-              "The King of Winter"/*name*/, posX, posY, initialGround);
+        super(health, strength, intelligence, defence, level, accuracy,"frost" , 
+              "The King of Winter", posX, posY, initialGround);
     }
-
-    /*attack
-     * the melee attack for the boss
-     * @param int strength the physical attacks
-     * @param int intelligence the value of ranged attacks
-     * @return void
+    
+    /**
+     * Interact method with the world
+     * @param world array, current enemy position x and y
      */
-    public void attack(int strength, int intelligence){
-        // Stronger than poison bosss
-        // not every attack is a freezing attack but freezing causes a slowdown in the player(either move speed or attack speed)
-        // 2 seconds between attacks
-    }
-
-        /**
-* move
-* This method accepts a world, x Coord and a y coord as a parameter and moves the enemy.
-* method returns a void
-* @param A  world array that holds data representing the game map
-* @param B integer holding the x coordinate of the enemy
-* @param C integer holding the y coordinate of the enemy
-* @return void
-*/
-    public void move(){
-        //if he is close to the player, he will melee attack
-        //if he is farther from the player, he will used a ranged attack
-        //only ranged attack applies freezing effect
+    @Override
+    public void move(World[][] world, int coordX, int coordY){
+      for (int i=coordX-2; i < coordX+3; i++) {
+        for (int j=coordY-2; j < coordY+3; j++) {
+          if (world[i][j] instanceof Player) {
+            attack(world[i][j]);
+            return;
+          }
+        }
+      }
     }
 }
