e60fd4fa0c1cf7dd958277ca5d8d31344edd649e
AlbertQuon <albertq2001@hotmail.com>
2018-06-14 14:02:20 -0400

    Update PoisonSpider.java

--- PoisonSpider.java
+++ PoisonSpider.java
@@ -1,10 +1,15 @@
-/* class PoisonSpider
- * Aiden Gimpel
- * June 14th, 2018
- * object for the poison spider enemy
-*/
+/*
+ * [PoisonSpider.java]
+ * Poison spider enemy
+ * @author Aiden
+ * 05/30/2018
+ */
+
 class PoisonSpider extends Type{
-  
+  /**
+   * Poison spider constructor
+   * @param health, strength, intelligence, defence, level, accuracy, name, initial position x and y, initial spawn ground
+   */
   PoisonSpider(int health, int strength, int intelligence, int defence, int level, int accuracy, 
                String name,  int posX, int posY, World initialGround){
     super(health, strength, intelligence, defence, level, accuracy,"poison", name, posX, posY, initialGround);
@@ -12,16 +17,5 @@ class PoisonSpider extends Type{
     
   }
   
-      /**
-* attack
-* This method accepts a world object and allows the enemy to attack the player
-* the method returns a void
-* @param A  world array that holds data representing the game map
-* @return void
-*/
-  public void attack(int strength, int intelligence){
-    //if enemy hits the player, deal 10% damage to player(Tentative) and 
-    //deal a poison effect causing 1 damage over time for 5 hits
-    //once it attacks it will wait 1 second to attack again
-  }
+  
 }
