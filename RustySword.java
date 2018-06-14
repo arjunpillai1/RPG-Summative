05b3615e65058a4799aa1ca094dec88596154742
arjunpillai1 <forgames4812@hotmail.com>
2018-06-14 13:05:47 -0400

    Update RustySword.java

--- RustySword.java
+++ RustySword.java
@@ -1,11 +1,15 @@
-/* class RustySword
- * Johann Muth
+/* RustySword
+ * @author Johann Muth
+ * @author Arjun Pillai
  * May 23 2018
  * base level sword
  */
 
 class RustySword extends Sword{
   
+  /**
+   * Empty Constructor to give specific cost, attack strength, and speed
+   */
   RustySword(){
     super("Rusty sword", 50, 10, 1); //cost, attackBoost, speed
   }
