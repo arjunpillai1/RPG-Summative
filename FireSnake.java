73c72348ed14d4f7e91ffbf8701138f836142c28
AlbertQuon <albertq2001@hotmail.com>
2018-06-14 13:53:49 -0400

    Update FireSnake.java

--- FireSnake.java
+++ FireSnake.java
@@ -1,11 +1,15 @@
-/* class FireSnake
- * Aiden Gimpel
- * June 14th, 2018
- * object for the fire snake enemy
-*/
+/**
+ * [FireSnake.java]
+ * The fire snake enemy
+ * @author Aiden, Albert
+ * 05/30/2018
+ */
 class FireSnake extends Type{
   
-  
+  /**
+   * Default constructor for the fire snakes
+   * @param health, strength, intelligence, defence, level, accuracy, initial position x and y, initial spawn ground
+   */
   FireSnake(int health, int strength, int intelligence, int defence, int level, int accuracy, 
            String name,  int posX, int posY, World initialGround){
     super(health, strength, intelligence, defence, level, accuracy,"fire", name, posX, posY, initialGround);
@@ -13,18 +17,7 @@ class FireSnake extends Type{
     setLvl( ((int)(Math.random()*5)+10));//between 10 and 14
   }
   
-          /**
-* attack
-* This method accepts a world object and allows the enemy to attack the player
-* the method returns a void
-* @param A  world array that holds data representing the game map
-* @return void
-*/
-      public void attack(int strength, int intelligence){
-      //if enemy hits the player, deal 10% damage to player(Tentative) and
-      //deal a burn effect causing  1 damage over time for 5 hits
-      //once it attacks once it will wait 1 second to attack again
-  }
+
       
       
 
