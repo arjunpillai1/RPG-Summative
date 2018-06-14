e19af8f99b7cde081dd27f7b23b98696d697f1bd
AlbertQuon <albertq2001@hotmail.com>
2018-06-14 14:01:39 -0400

    Update PoisonSnake.java

--- PoisonSnake.java
+++ PoisonSnake.java
@@ -1,26 +1,21 @@
-/* class PoisonSnake
- * Aiden Gimpel
- * June 14th, 2018
- * object for the poison snake enemy
-*/
+/*
+ * [PoisonSnake.java]
+ * Poison Snake enemy
+ * @author Aiden
+ * 05/30/2018
+ */
+
 class PoisonSnake extends Type{
-  
+  /**
+   * Poison snake constructor
+   * @param health, strength, intelligence, defence, level, accuracy, name, initial position x and y, initial spawn ground
+   */
   PoisonSnake(int health, int strength, int intelligence, int defence, int level, int accuracy, 
            String name,  int posX, int posY, World initialGround){
     super(health, strength, intelligence, defence, level, accuracy,"poison", name, posX, posY, initialGround);
     setLvl(((int)(Math.random()*3)+3));
   }
   
-      /**
-* attack
-* This method accepts a world object and allows the enemy to attack the player
-* the method returns a void
-* @param A  world array that holds data representing the game map
-* @return void
-*/
-  public void attack(int strength, int intelligence){
-    //if enemy hits the player, deal 10% damage to player(Tentative) and 
-    //deal a poison effect causing 1 damage over time for 5 hits
-    //once it attacks it will wait 1 second to attack again
-  }
+  
+
 }
